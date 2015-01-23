e1176079a1e5503e50347e251834217207826aeb
Daniel Moran <dmj@tid.es>
2015-01-23 14:42:17 +0100

    FIX Linter errors

--- test/unit/iotagent-thinking-things_test.js
+++ test/unit/iotagent-thinking-things_test.js
@@ -176,13 +176,13 @@ describe('Southbound measure reporting', function() {
                 url: 'http://localhost:' + config.thinkingThings.port + config.thinkingThings.root,
                 method: 'POST',
                 form: {
-                    "cadena": "#ITgAY," +
-                        "#0,P1,214,07,b00,444,-47," +
-                        "#0,K1,300$," +
-                        "#3,B,4.70,1,1,1,1,0,-1$" +
-                        "#4,T1,31.48,0$" +
-                        "#4,H1,31.48,1890512.00,0$" +
-                        "#4,LU,142.86,0$"
+                    'cadena': '#ITgAY,' +
+                        '#0,P1,214,07,b00,444,-47,' +
+                        '#0,K1,300$,' +
+                        '#3,B,4.70,1,1,1,1,0,-1$' +
+                        '#4,T1,31.48,0$' +
+                        '#4,H1,31.48,1890512.00,0$' +
+                        '#4,LU,142.86,0$'
                 }
             };
 
