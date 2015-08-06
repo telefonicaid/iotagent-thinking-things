338a9177f32ab8428db16f9fde822bbd09bff49a
Daniel Moran <dmj@tid.es>
2015-08-06 11:08:33 +0200

    ADD Docker configuration

---
+++ config-docker.js
@@ -0,0 +1,44 @@
+var config = {};
+
+config.thinkingThings = {
+    logLevel: 'ERROR',
+    port: 8000,
+    root: '/thinkingthings',
+    sleepTime: 300
+};
+
+config.ngsi = {
+    logLevel: 'ERROR',
+    defaultType: 'ThinkingThing',
+    contextBroker: {
+        host: 'orion',
+        port: '1026'
+    },
+    server: {
+        port: 4041
+    },
+    deviceRegistry: {
+        type: 'memory'
+    },
+    types: {
+        'ThinkingThing': {
+            service: 'smartGondor',
+            subservice: '/gardens',
+            type: 'ThinkingThing',
+            commands: [],
+            lazy: [],
+            active: [
+                {
+                    name: 'humidity',
+                    type: 'Number'
+                }
+            ]
+        }
+    },
+    service: 'smartGondor',
+    subservice: '/gardens',
+    providerUrl: 'http://127.0.0.1:4041',
+    deviceRegistrationDuration: 'P1M'
+};
+
+module.exports = config;
