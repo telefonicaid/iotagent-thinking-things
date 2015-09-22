5fc83b5c162a1b1a8ec6afc84bfa39fed30b2378
Daniel Moran <dmj@tid.es>
2015-09-22 10:55:13 +0200

    FIX 200OK to 200 OK in test descriptions

--- test/unit/iotagent-thinking-things_test.js
+++ test/unit/iotagent-thinking-things_test.js
@@ -54,7 +54,7 @@ describe('Southbound measure reporting', function() {
         it('should update the device entity in the Context Broker with the humidity attribute',
             utils.checkContextBroker(options));
 
-        it('should return a 200OK with the appropriate response: ',
+        it('should return a 200 OK with the appropriate response: ',
             utils.checkResponse(options, '#STACK1#953E78F,H1,-1$condition,'));
     });
 
@@ -75,7 +75,7 @@ describe('Southbound measure reporting', function() {
             it('should update the device entity in the Context Broker with the humidity attribute',
                 utils.checkContextBroker(options));
 
-            it('should return a 200OK with the appropriate response: ',
+            it('should return a 200 OK with the appropriate response: ',
                 utils.checkResponse(options, '#STACK1#673495,T1,-1$theCondition,'));
     });
     describe('When a GPS measure arrives to the IoT Agent: #STACK1#5143,GPS,21.1,-9.4,12.3,0.64,127,12$cond1,',
@@ -95,7 +95,7 @@ describe('Southbound measure reporting', function() {
         it('should update the device entity in the Context Broker with the humidity attribute',
             utils.checkContextBroker(options));
 
-        it('should return a 200OK with the appropriate response: ',
+        it('should return a 200 OK with the appropriate response: ',
             utils.checkResponse(options, '#STACK1#5143,GPS,-1$cond1,'));
     });
     describe('When a request arrives to the IoT Agent having two modules', function() {
@@ -113,7 +113,7 @@ describe('Southbound measure reporting', function() {
 
         it('should update the device entity in the Context Broker with both attributes',
             utils.checkContextBroker(options));
-        it('should return a 200OK with the appropriate response',
+        it('should return a 200 OK with the appropriate response',
             utils.checkResponse(options, '#STACK1#5143,GPS,-1$cond1,#673495,T1,-1$theCondition,'));
     });
     describe('When a request arrives to the IoT Agent having the Core Module', function() {
@@ -131,7 +131,7 @@ describe('Southbound measure reporting', function() {
 
         it('should update the device entity in the Context Broker with both attributes',
             utils.checkContextBroker(options));
-        it('should return a 200OK with the configured sleep time in the core module',
+        it('should return a 200 OK with the configured sleep time in the core module',
             utils.checkResponse(options, '#STACK1#5143,GPS,-1$cond1,#673495,K1,300$theCondition,'));
     });
 
@@ -161,7 +161,7 @@ describe('Southbound measure reporting', function() {
 
         it('should update the device entity in the Context Broker with both attributes',
             utils.checkContextBroker(options));
-        it('should return a 200OK with the current value of the configuration parameter read from the CB',
+        it('should return a 200 OK with the current value of the configuration parameter read from the CB',
             utils.checkResponse(options, '#STACK1#0,GC,conf,44,-1$,#1,GC,conf2,456,-1$,#673495,K1,300$theCondition,'));
     });
 
@@ -191,7 +191,7 @@ describe('Southbound measure reporting', function() {
 
         it('should update the device entity in the Context Broker with both attributes',
             utils.checkContextBroker(options));
-        it('should return a 200OK with the current value of the configuration parameter read from the CB',
+        it('should return a 200 OK with the current value of the configuration parameter read from the CB',
             utils.checkResponse(options, '#STACK1#1,AV,2:5:1000:1Cb.C,-1$,#673495,K1,300$theCondition,'));
     });
 
@@ -247,7 +247,7 @@ describe('Southbound measure reporting', function() {
         it('should update the device entity in the Context Broker with the humidity attribute',
             utils.checkContextBroker(options));
 
-        it('should return a 200OK with the appropriate response: ',
+        it('should return a 200 OK with the appropriate response: ',
             utils.checkResponse(options, '#ITgAY#0,P1,-1$,#0,K1,300$,#3,B,1,1,0,-1$,#4,T1,-1$,#4,H1,-1$,#4,LU,-1$,'));
     });
     describe('When the plainFormat configuration flag is set', function() {
@@ -279,7 +279,7 @@ describe('Southbound measure reporting', function() {
         it('should update the device entity in the Context Broker with the humidity attribute',
             utils.checkContextBroker(options));
 
-        it('should return a 200OK with the appropriate response: ',
+        it('should return a 200 OK with the appropriate response: ',
             utils.checkResponse(options, '#ITgAY#0,P1,-1$,#0,K1,300$,#3,B,1,1,0,-1$,#4,T1,-1$,#4,H1,-1$,#4,LU,-1$,'));
     });
 
